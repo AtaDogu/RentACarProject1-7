718905c9cfd5fb248e1be315908d16dd809b79fc
AtaDogu <88247572+AtaDogu@users.noreply.github.com>
2021-08-10 21:10:45 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,82 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Entity.DTOs;
+using Core.Utilities.Results;
+using Business.Constants;
+using FluentValidation;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Aspects.Autofac.Validation;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public IDataResult<List<Car>> GetAll()
+        {
+            if (DateTime.Now.Hour==14)
+            {
+                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarsListed);
+        }
+
+        public IDataResult<List<Car>> GetByDailyPrice(decimal min, decimal max)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.DailyPrice > min && p.DailyPrice < max));
+        }
+
+        public IDataResult<List<Car>> GetAllByBrandId(int Id)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.BrandId == Id));
+        }
+
+        public IDataResult<List<Car>> GetCarsByColourId(int Id)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColourId == Id));
+        }
+
+        [ValidationAspect(typeof(CarValidator))]
+        public IResult Add(Car car)
+        {
+
+           
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
+
+
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
+        }
+
+        public IResult Update(Car car)
+        {
+            _carDal.Update(car);
+            return new SuccessResult(Messages.CarUpdated);
+        }
+
+        public IResult Remove(Car car)
+        {
+            _carDal.Remove(car);
+            return new SuccessResult(Messages.CarRemoved);
+        }
+
+        public IDataResult<Car> GetById(int Id)
+        {
+            return new SuccessDataResult<Car>(_carDal.Get(p => p.Id == Id));
+        }
+    }
+}
